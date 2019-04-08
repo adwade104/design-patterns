4ca035cafc691172d84ba4f9bfe5b2a338809a93
adwade787 <adwade787@gmail.com>
2019-04-07 20:25:17 -0400

    Added mediator pattern examples

---
+++ src/main/java/com/wade/adam/pattern/mediator/banas/Colleague.java
@@ -0,0 +1,29 @@
+package com.wade.adam.pattern.mediator.banas;
+
+public abstract class Colleague {
+
+    private Mediator mediator;
+    private int colleagueCode;
+
+    public Colleague(Mediator mediator) {
+        this.mediator = mediator;
+        mediator.addColleague(this);
+    }
+
+    public void saleOffer(String stock, int shares){
+
+        mediator.saleOffer(stock, shares, colleagueCode);
+
+    }
+
+    public void buyOffer(String stock, int shares){
+
+        mediator.buyOffer(stock, shares, colleagueCode);
+
+    }
+
+    public void setColleagueCode(int colleagueCode) {
+        this.colleagueCode = colleagueCode;
+    }
+
+}
