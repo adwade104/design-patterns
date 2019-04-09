fd428c67992c0f8b20f8e73112f7ffcc6e7c0ced
adwade787 <adwade787@gmail.com>
2019-04-08 21:54:07 -0400

    Added memento pattern examples

---
+++ src/main/java/com/wade/adam/pattern/memento/banas/Memento.java
@@ -0,0 +1,15 @@
+package com.wade.adam.pattern.memento.banas;
+
+public class Memento {
+
+    private String article;
+
+    public Memento(String article) {
+        this.article = article;
+    }
+
+    public String getArticle() {
+        return article;
+    }
+
+}
---
+++ src/main/java/com/wade/adam/pattern/memento/wiki/Originator.java
@@ -0,0 +1,22 @@
+package com.wade.adam.pattern.memento.wiki;
+
+public class Originator {
+
+    private String state;
+
+    public void setState(String state) {
+        this.state = state;
+        System.out.println("Originator: Setting state to " + state);
+    }
+
+    public Memento saveToMemento() {
+        System.out.println("Originator: Saving to Memento");
+        return new Memento(this.state);
+    }
+
+    public void restoreFromMemento(Memento memento) {
+        this.state = memento.getSavedState();
+        System.out.println("Originator: State after restoring from Memento: " + state);
+    }
+
+}
